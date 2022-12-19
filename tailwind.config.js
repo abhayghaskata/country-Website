15cbdf13da616a02da787117ece11ff5fd119a6a
abhayghaskata <106431995+abhayghaskata@users.noreply.github.com>
2022-12-19 10:56:16 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  future: {},
+  purge: [require('tailwindcss'), require('autoprefixer')],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
